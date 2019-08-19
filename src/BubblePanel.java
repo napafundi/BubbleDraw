125b2a8347b129c71d851bfc37b4820a1ba541ad
Nick Pafundi <napafundi@gmail.com>
2019-08-19 15:58:51 -0400

    completed click to draw, mouseWheel to enlarge and click-and-drag drawing features

--- src/BubblePanel.java
+++ src/BubblePanel.java
@@ -3,10 +3,47 @@ import java.awt.Color;
 import java.util.Random;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.awt.event.*;
 public class BubblePanel extends JPanel{
     Random rand = new Random();
     ArrayList<Bubble> bubbleList;
     int size = 25;
+    public BubblePanel() {
+        bubbleList = new ArrayList<Bubble>();
+        setBackground(Color.BLACK);
+        //testBubbles();
+        addMouseListener(new BubbleListener());
+        addMouseMotionListener(new BubbleListener());
+        addMouseWheelListener(new BubbleListener());
+    }
+    public void paintComponent(Graphics canvas) {
+        super.paintComponent(canvas);
+        for (Bubble b: bubbleList) {
+            b.draw(canvas);
+        }
+    }
+    public void testBubbles() {
+        for (int n = 0; n < 100; n++) {
+            int x = rand.nextInt(600);
+            int y = rand.nextInt(400);
+            int size = rand.nextInt(50);
+            bubbleList.add(new Bubble(x, y, size));
+        }
+        repaint();
+    }
+    private class BubbleListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            bubbleList.add(new Bubble(e.getX(), e.getY(), size));
+            repaint();
+        }
+        public void mouseDragged(MouseEvent e) {
+            bubbleList.add(new Bubble(e.getX(), e.getY(), size));
+            repaint();
+        }
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            size += e.getUnitsToScroll();
+        }
+    }
     private class Bubble {
         private int x;
         private int y;
@@ -25,4 +62,7 @@ public class BubblePanel extends JPanel{
             canvas.fillOval(x - size/2, y-size/2, size, size);
         }
     }
+
 }
+
+
